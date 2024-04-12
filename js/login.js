931b95e88702ce5dd8ff3f1d4db8b4b45afd63f3
AungKaungKyaw <aungakk619@gmail.com>
2024-04-12 07:37:50 +0630

    second

--- js/login.js
+++ js/login.js
@@ -1,3 +1,18 @@
-$.ajax({
-    url:''
+$('#first').focus()
+$('input').focus(function(){
+    $(this).css('background-color', '#efefef')
+})
+$('input').blur(function(){
+    $(this).css('background-color', 'white')
+})
+$('.myclass').click(function() { $(this).slideUp() });
+$(document).keypress(function(event){
+    key= String.fromCharCode(event.which)
+    if(key >= 'a' && key <= 'z' ||
+        key >= 'A' && key <= 'Z' ||
+        key >= '0' && key <= '9')
+    {
+        $('#result').html("You pressed : " + key)
+        event.preventDefault();
+    }
 })
