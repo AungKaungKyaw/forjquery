29df82dec6d20e208c88e41c9e4165d89117f788
AungKaungKyaw <aungakk619@gmail.com>
2024-04-12 08:46:40 +0630

    asdf

--- js/login.js
+++ js/login.js
@@ -16,3 +16,16 @@ $(document).keypress(function(event){
         event.preventDefault();
     }
 })
+canvas  = $('#pad')[0]
+context = canvas.getContext("2d")
+pendown = false
+$('#pad').mousemove(function(event)
+{
+    var xpos = event.pageX - canvas.offsetLeft
+    var ypos = event.pageY - canvas.offsetTop
+    if (pendown) context.lineTo(xpos, ypos)
+    else         context.moveTo(xpos, ypos)
+    context.stroke()
+})
+$('#pad').mousedown(function() { pendown = true  } )
+$('#pad')  .mouseup(function() { pendown = false } )
